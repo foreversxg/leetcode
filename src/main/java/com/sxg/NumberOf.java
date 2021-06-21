83517da610541600bb32f9e283de1be7ce66875b
shaoxiangen <shaoxiangen@corp.netease.com>
2021-06-21 21:15:27 +0800

    init

---
+++ src/main/java/com/sxg/NumberOf.java
@@ -0,0 +1,27 @@
+package com.sxg;
+
+/**
+ *  二进制中1的个数
+ * @author shaoxiangen
+ * create in 2021/1/25
+ */
+public class NumberOf {
+
+    public static void main(String[] args) {
+        exe(3);
+    }
+
+    public static void exe(int n ) {
+        if(n== 0) {
+            return;
+        }
+        int count = 0;
+        while (n > 0 || -n> 0 ) {
+            if((n & 1) == 1) {
+             count ++;
+            }
+            n =n >> 1;
+        }
+        System.out.println(count);
+    }
+}
